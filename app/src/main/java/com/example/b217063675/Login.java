1eb7af41fa8aae882c7af392ef7ec22416286606
uwiduhayeclaudine <uwiduhayeclaudine@gmail.com>
2019-05-31 11:16:07 +0200

    first commit

--- app/src/main/java/com/example/b217063675/Login.java
+++ app/src/main/java/com/example/b217063675/Login.java
@@ -1,7 +1,11 @@
 package com.example.b217063675;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            Toast.makeText(getApplicationContext(), "logout succefully", Toast.LENGTH_LONG).show();
+            Intent me=new Intent(Login.this,MainActivity.class);
+            startActivity(me);
+        } else if (id == android.R.id.home) {
+            finish();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/b217063675/Sarah.java
+++ app/src/main/java/com/example/b217063675/Sarah.java
@@ -3,9 +3,11 @@ package com.example.b217063675;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Sarah extends AppCompatActivity {
 
@@ -16,16 +18,20 @@ public class Sarah extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sarah);
 
-        user = (EditText) findViewById(R.id.username);
-        password =(EditText) findViewById(R.id.password);
-        register = (Button) findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent inti = new Intent(Sarah.this,MainActivity.class);
-                startActivity(inti);
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            Toast.makeText(getApplicationContext(), "logout succefully", Toast.LENGTH_LONG).show();
+        } else if (id == android.R.id.home) {
+            finish();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
